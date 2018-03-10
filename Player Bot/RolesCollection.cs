92fc6c83f98db368e18188bae022900824a5f0dc
SuuperW <SuuperW@gmail.com>
2018-03-10 17:16:45 -0600

    add RolesCollection.cs

---
+++ Player Bot/RolesCollection.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Newtonsoft.Json.Linq;
+
+namespace Player_Bot
+{
+    class RolesCollection
+    {
+        public SortedSet<ulong> publicRoles;
+        public SortedSet<ulong> pr2GuildRoles;
+
+        public RolesCollection(JObject obj)
+        {
+            publicRoles = new SortedSet<ulong>();
+            foreach (JToken token in (JArray)obj["public"])
+                publicRoles.Add((ulong)token);
+            pr2GuildRoles = new SortedSet<ulong>();
+            foreach (JToken token in (JArray)obj["pr2_guilds"])
+                pr2GuildRoles.Add((ulong)token);
+        }
+
+        public void Save(string path)
+        {
+            JObject obj = new JObject();
+            obj["public"] = JArray.FromObject(publicRoles);
+            obj["pr2_guilds"] = JArray.FromObject(pr2GuildRoles);
+
+            System.IO.File.WriteAllText(path, obj.ToString());
+        }
+    }
+}
